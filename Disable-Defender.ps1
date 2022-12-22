7fa1c7bd05ff8561d1b7d3e1fcd284d17b6fa335
mutiny1 <mutiny1@gmail.com>
2022-12-21 22:32:17 -0600

    Delete Disable-Defender.ps1

--- Disable-Defender.ps1
+++
@@ -1,3 +0,0 @@
-Set-MpPreference -DisableRealtimeMonitoring $true
-New-ItemProperty -Path “HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender” -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
-Uninstall-WindowsFeature -Name Windows-Defender
