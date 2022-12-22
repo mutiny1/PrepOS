019222cd0d14d46e8fd36fd495f4ded502d10142
mutiny1 <mutiny1@gmail.com>
2022-12-21 23:59:12 -0600

    Update Checklist-Admin-Station.ps1

--- Required/Checklist-Admin-Station.ps1
+++ Required/Checklist-Admin-Station.ps1
@@ -1,3 +1,10 @@
+# Required for WinRM
 Set-NetConnectionProfile -NetworkCategory Private
 winrm quickconfig
 Set-Item wsman:\localhost\client\TrustedHosts -Value *
+
+# Install PSWindowsUpdate module
+# on management station
+# AsAdmin
+Install-Module PSWindowsUpdate -Force
+
